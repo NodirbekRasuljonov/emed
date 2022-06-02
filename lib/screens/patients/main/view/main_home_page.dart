ff15395a2abe0ac3559a0ca69fbc7cea813e549c
Nodirbek Rasuljonov <nodirbekmaqsudjonovich@gmail.com>
2022-06-02 15:14:26 +0500

    bootombars used with bloc

--- lib/screens/patients/main/view/main_home_page.dart
+++ lib/screens/patients/main/view/main_home_page.dart
@@ -3,11 +3,12 @@ import 'package:emed/core/extensions/size_extension.dart';
 import 'package:emed/screens/patients/auth/signup/widgets/my_elevatedButton.dart';
 import 'package:emed/screens/patients/main/cubit/home_cubit.dart';
 import 'package:emed/screens/patients/main/state/home_State.dart';
+import 'package:emed/screens/patients/treatments/treatment_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:on_click/extensions/click_extension.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:on_click/on_click.dart';
 
 class PatientsHomePage extends StatefulWidget {
   PatientsHomePage({Key? key}) : super(key: key);
@@ -17,42 +18,92 @@ class PatientsHomePage extends StatefulWidget {
 }
 
 class _PatientsHomePageState extends State<PatientsHomePage> {
-  int currentindex = 0;
 
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<HomePatientsCubit, HomePatientsState>(
         builder: (context, state) {
-          return Scaffold(
+         
+            return Scaffold(
             appBar: mainAppBar(),
             body: mainBody(context),
-            bottomNavigationBar: Container(
-              height: context.h * 0.07,
-              width: context.h,
-              color: Colors.red.withOpacity(0.6),
-              alignment: Alignment.center,
-              child: mainBottom(context),
-            ),
+            bottomNavigationBar: mainBottomNavigationBar(context),
           );
         },
         listener: (context, state) {});
   }
 
-  Row mainBottom(context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        bars(
-            label: 'Home',
-            iconData: SvgPicture.asset('assets/svg/home/home_icon.svg'),
-            index: 0,
-            context: context),
-        bars(
-            label: 'Treatments',
-            iconData: Image.asset('assets/images/injection.png'),
-            index: 1,
-            context: context),
-      ],
+  SizedBox mainBottomNavigationBar(BuildContext context) {
+    return SizedBox(
+            height: context.h * 0.07,
+            width: context.h,
+            child: Row(
+              children: [
+                bars(
+                    icon: Icon(
+                      Icons.home,
+                      color: context.watch<HomePatientsCubit>().currentindex == 0
+                          ? ColorsConst.kPrimaryColor
+                          : ColorsConst.textColor,
+                    ),
+                    index: 0,
+                    label: 'Home'),
+                bars(
+                    label: 'Treatments',
+                    icon: Icon(
+                      Icons.medication_rounded,
+                      color: context.watch<HomePatientsCubit>().currentindex == 1
+                          ? ColorsConst.kPrimaryColor
+                          : ColorsConst.textColor,
+                    ),
+                    index: 1),
+                bars(
+                    label: 'Doctors',
+                    icon: Icon(
+                      Icons.medical_information_outlined,
+                      color: context.watch<HomePatientsCubit>().currentindex == 2
+                          ? ColorsConst.kPrimaryColor
+                          : ColorsConst.textColor,
+                    ),
+                    index: 2),
+                bars(
+                    label: 'Hospitals',
+                    icon: Icon(
+                      Icons.local_hospital_sharp,
+                      color: context.watch<HomePatientsCubit>().currentindex == 3
+                          ? ColorsConst.kPrimaryColor
+                          : ColorsConst.textColor,
+                    ),
+                    index: 3),
+              ],
+            ),
+          );
+  }
+
+  Expanded bars(
+      {required String label, required Icon icon, required int index}) {
+    return Expanded(
+      child: SizedBox(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            icon,
+            Text(
+              label,
+              style: TextStyle(
+                color: context.watch<HomePatientsCubit>().currentindex == index
+                    ? ColorsConst.kPrimaryColor
+                    : ColorsConst.textColor,
+                fontSize: 12,
+              ),
+            ),
+          ],
+        ),
+      ).onClick(
+        () {
+          context.read<HomePatientsCubit>().changetheIndex(index: index);
+        },
+      ),
     );
   }
 
@@ -80,6 +131,7 @@ class _PatientsHomePageState extends State<PatientsHomePage> {
               height: 30.0,
             ),
             MyElevatedBUtton(
+              
                 title: 'Add new appointment',
                 route: '/addappointment',
                 context: context),
@@ -106,37 +158,5 @@ class _PatientsHomePageState extends State<PatientsHomePage> {
       centerTitle: true,
       title: SvgPicture.asset('assets/svg/home_icon.svg'),
     );
-  }
-
-  Widget bars(
-      {required String label,
-      required Widget iconData,
-      required int index,
-      required BuildContext context}) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        iconData,
-        Text(
-          label,
-          style: TextStyle(
-            color: currentindex == index
-                ? ColorsConst.kPrimaryColor
-                
-                : ColorsConst.textColor,
-            fontSize: 12,
-          ),
-        ),
-      ],
-    ).onClick(() {
-      
-      setState(() {
-        currentindex==index;
-        
-      });
-  
-     
-      
-    });
   }
 }
